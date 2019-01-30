597c12d764cbb5f3b1edbf40704ef236a5ec312d
Maximiliano Usich <maximilianou@gmail.com>
2019-01-30 14:03:04 -0300

    Initial Browser Test with Puppeteer

---
+++ frontest/test/index.spec.js
@@ -0,0 +1,41 @@
+const assert    = require('assert');
+const puppeteer = require('puppeteer');
+describe("Browsing with puppeteer:", ()=> {
+  describe("Geting one page", ()=>{
+    it("Check OK:",  async ()=> {
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        });    
+        const page = await browser.newPage();
+        await page.goto('https://bitbucket.org/maximilianou/'); //, {waitUntil: 'networkidle2'});    
+        let textResponse = await page.evaluate(() => {
+          //<a href="/maximilianou/">Maximiliano Usich</a>
+          let elemCheck = document.querySelector('a[href="/maximilianou/"]');
+          return elemCheck.innerHTML;
+        });
+        assert( textResponse === 'Maximiliano Usich', "[OK] Content Found" );
+        browser.close();
+    });
+    it("Check Not Found:", async ()=> {
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        });    
+        const page = await browser.newPage();
+        await page.goto('https://bitbucket.org/maximilianou/UPS!!');// , {waitUntil: 'networkidle2'});    
+        let textResponse = await page.evaluate(() => {
+          //<a href="/maximilianou/">Maximiliano Usich</a>
+          let elemCheck = document.querySelector('a[href="/maximilianou/"]');
+          return elemCheck;
+        });
+        assert( textResponse === null, "[OK] Content Not Found" );
+        browser.close();
+        
+      });
+  });
+});
